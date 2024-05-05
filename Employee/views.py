0f38b34314bb301c9d85fe034101c316d11c66f4
sindhuvarkole <sindhuvarkol1810@gmail.com>
2024-05-05 21:21:19 +0530

    adding to repo

---
+++ Employee/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+from salary.models import Employee # type: ignore
+
+def home(request):
+    employees=Employee.objects.all()
+    context={
+        'employees':employees,
+    }
+    return render(request,'home.html',context)
